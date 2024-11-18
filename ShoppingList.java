fb28f83f3607afb6952724c9c4d987a48be706d1
TMacRae2026 <tmacrae2026@cchsdons.com>
2024-11-18 10:50:36 -0800

    Create ShoppingList.java

---
+++ ShoppingList.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shoppinglist;
+
+/**
+ *
+ * @author TMacRae2026
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ShoppingList {
+    public static void main(String[] args) {
+        ArrayList<String> shoppingList = new ArrayList<String>();
+        
+        
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the item you would like to add by typeing 'add (item)'. (type 'help' to get a list of all commands):");
+            String command = scanner.next(); //two inputs, the command name and the argument for the command
+            String arg = "";
+            if(command.equalsIgnoreCase("add") || command.equalsIgnoreCase("remove")) {
+                arg = scanner.next();
+            }
+            switch(command.toLowerCase()) {
+                case "add":
+                    shoppingList.add(arg);
+                    break;
+                case "remove":
+                    for(int i = 0; i < shoppingList.size(); i++) {
+                        if(shoppingList.get(i).equalsIgnoreCase(arg)) { //linar search the list to find the item.
+                            shoppingList.remove(i);
+                        }
+                    }
+                    break;
+                case "view": //print the whole list
+                    int i = 0;
+                    for(String s : shoppingList) {
+                        i++;
+                        System.out.println(i + ": " + s);
+                    }
+                    break;
+                case "help":
+                    printHelp();
+                    break;
+            }
+            
+        }
+    }
+    
+    static void printHelp() {
+        System.out.println("add (item), adds an item to the list.\nremove (item), removes an item from the list.\nhelp, displays this list.\nview, prints the list.");
+    }
+    
+}
