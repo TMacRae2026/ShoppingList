0ae1dec2ae96952057c16a465e17bc832bf71d7f
TMacRae2026 <tmacrae2026@cchsdons.com>
2024-11-20 08:48:51 -0800

    Update ShoppingList.java to be object oriented

--- ShoppingList.java
+++ ShoppingList.java
@@ -14,44 +14,116 @@ import java.util.Scanner;
 public class ShoppingList {
     public static void main(String[] args) {
         ArrayList<String> shoppingList = new ArrayList<String>();
+        ArrayList<Command> commands = new ArrayList<Command>();
+        commands.add(new Add()); //makes a list of all command instances
+        commands.add(new Remove());
+        commands.add(new View());
+        commands.add(new Help());
         
         
         while(true) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter the item you would like to add by typeing 'add (item)'. (type 'help' to get a list of all commands):");
-            String command = scanner.next(); //two inputs, the command name and the argument for the command
+            String command = scanner.next();
             String arg = "";
-            if(command.equalsIgnoreCase("add") || command.equalsIgnoreCase("remove")) { //Argument only applies to add and remove cases
+            if(command.equalsIgnoreCase("add") || command.equalsIgnoreCase("remove")) {
                 arg = scanner.next();
             }
-            switch(command.toLowerCase()) { //note for future command with arg idea, make a class called 'command' and make classes that extend command
-                case "add":
-                    shoppingList.add(arg);
-                    break;
-                case "remove":
-                    for(int i = 0; i < shoppingList.size(); i++) {
-                        if(shoppingList.get(i).equalsIgnoreCase(arg)) { //linar search the list to find the item.
-                            shoppingList.remove(i);
-                        }
-                    }
-                    break;
-                case "view": //print the whole list
-                    int i = 0;
-                    for(String s : shoppingList) {
-                        i++;
-                        System.out.println(i + ": " + s);
-                    }
-                    break;
-                case "help":
-                    printHelp();
-                    break;
+            for(Command c : commands) { //Executes user inputted command
+                if(c.getName().equals(command)){
+                    shoppingList = c.executeCommand(arg, shoppingList);
+                }
             }
-            
         }
     }
-    
-    static void printHelp() {
-        System.out.println("add (item), adds an item to the list.\nremove (item), removes an item from the list.\nhelp, displays this list.\nview, prints the list.");
+}
+
+class Command {
+    // Fields
+    private String name;
+
+    // Constructor
+    public Command(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
     
+    // Method to be overridden by subclasses
+    public ArrayList<String> executeCommand(String arg, ArrayList<String> list) {
+        return null;
+    }
 }
+
+
+class Add extends Command {
+
+    // Constructor
+    public Add() {
+        super("add"); //supercall
+    }
+    // Overridden method to implement specific behavior
+    @Override
+    public ArrayList<String> executeCommand(String arg, ArrayList<String> in) {
+        in.add(arg);
+        return in;
+    }
+}
+
+class Remove extends Command {
+
+    // Constructor
+    public Remove() {
+        super("remove"); //supercall
+    }
+
+    // Overridden method to implement specific behavior
+    @Override
+    public ArrayList<String> executeCommand(String arg, ArrayList<String> in) {
+        in.remove(arg);
+        return in;
+    }
+}
+
+class View extends Command {
+
+    // Constructor
+    public View() {
+        super("view"); //supercall
+    }
+
+    // Overridden method to implement specific behavior
+    @Override
+    public ArrayList<String> executeCommand(String arg, ArrayList<String> in) {
+        int i = 0;
+        for(String s : in) {
+            i++;
+            System.out.println(i + ": " + s);
+        }
+        return in;
+    }
+}
+
+class Help extends Command {
+
+    // Constructor
+    public Help() {
+        super("help"); //supercall
+    }
+
+    // Overridden method to implement specific behavior
+    @Override
+    public ArrayList<String> executeCommand(String arg, ArrayList<String> in) {
+        System.out.println("add (item), adds an item to the list."
+                + "\nremove (item), removes an item from the list."
+                + "\nhelp, displays this list."
+                + "\nview, prints the list."); 
+        /*Note, how do I get access to the commands arraylist?
+        I don't want to make the executeCommand have another argument,
+        but I don't see any other way to do it.*/
+        return in;
+    }
+}
+
