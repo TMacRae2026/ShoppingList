475ccba65dcab594f09696f4902664ab344704a1
TMacRae2026 <tmacrae2026@cchsdons.com>
2024-11-20 08:06:46 -0800

    Update ShoppingList.java

--- ShoppingList.java
+++ ShoppingList.java
@@ -21,10 +21,10 @@ public class ShoppingList {
             System.out.println("Enter the item you would like to add by typeing 'add (item)'. (type 'help' to get a list of all commands):");
             String command = scanner.next(); //two inputs, the command name and the argument for the command
             String arg = "";
-            if(command.equalsIgnoreCase("add") || command.equalsIgnoreCase("remove")) {
+            if(command.equalsIgnoreCase("add") || command.equalsIgnoreCase("remove")) { //Argument only applies to add and remove cases
                 arg = scanner.next();
             }
-            switch(command.toLowerCase()) {
+            switch(command.toLowerCase()) { //note for future command with arg idea, make a class called 'command' and make classes that extend command
                 case "add":
                     shoppingList.add(arg);
                     break;
